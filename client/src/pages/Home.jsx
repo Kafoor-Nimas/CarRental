555eb32e7d856325965801a013b14c085538d2dc
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-08 10:37:35 +0530

    complete testimonial section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "../components/Hero";
 import FeaturedSection from "../components/FeaturedSection";
 import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedSection />
-      <Banner/>
+      <Banner />
+      <Testimonial />
     </div>
   );
 };
