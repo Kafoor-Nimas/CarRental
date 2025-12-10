e37b439ec1b029fc7421ea0217d49e27ea918dcd
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-10 15:00:16 +0530

    created a controller function to register user

---
+++ server/controllers/userController.js
@@ -0,0 +1,32 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//Genarate JWT Token
+const generateToken = (userId) => {
+  const payload = userId;
+  return jwt.sign(payload, process.env.JWT_SECRET);
+};
+
+
+//register user
+export const regiterUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password || password.length < 8) {
+      return res.json({ success: false, message: "Fill all the fields" });
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = generateToken(user._id.toString());
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
