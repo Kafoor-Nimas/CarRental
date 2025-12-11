a1c11fd42027e9e6b3601bb0a661b3f334ab9399
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-11 07:37:22 +0530

    created a multer middleware to upload image file in the request

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 
+//API to change role of user
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
@@ -11,4 +12,15 @@ export const changeRoleToOwner = async (req, res) => {
   }
 };
 
+//API to list car
 
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile =req.file;
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
