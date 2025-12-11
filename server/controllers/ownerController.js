a4c83a40a115854ce8c73cb2696a9c595b18805e
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-11 16:20:56 +0530

    create a API to list cars of specific user and API to toggle car availabilty

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,7 +1,6 @@
-
 import Car from "../models/car.js";
-import User from "../models/user.js";
 
+import User from "../models/user.js";
 
 //API to change role of user
 export const changeRoleToOwner = async (req, res) => {
@@ -23,7 +22,10 @@ export const addCar = async (req, res) => {
 
     // 1. Check if the file uploaded successfully
     if (!req.file) {
-      return res.json({ success: false, message: "Image upload failed or no image provided" });
+      return res.json({
+        success: false,
+        message: "Image upload failed or no image provided",
+      });
     }
 
     // 2. Get the Cloudinary URL
@@ -41,7 +43,39 @@ export const addCar = async (req, res) => {
     });
 
     res.json({ success: true, message: "Car Added", car: newCar });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+//API to list specific Owner Cars
+
+export const getOwnerCars = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to toggle Car Availability
+export const toggleCarAvailability = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
+
+    //Checking is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+    car.isAvailable = !car.isAvailable;
+    await car.save();
+    res.json({ success: true, message: "Availability Toggled" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
