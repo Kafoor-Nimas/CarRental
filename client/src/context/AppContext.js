ca8b2b99b4cd42d861152af277d4a0069161bc38
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-13 19:56:02 +0530

    connect backend with frontend basic setup

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,8 +1,16 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const value = {};
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
 };
