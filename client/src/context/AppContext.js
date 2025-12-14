32b4448a61d3ebcf712157fe4407bdff525f64e5
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-14 07:13:24 +0530

    added multiple state variables to AppContext

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,13 +1,41 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+
+  const [cars, setCars] = useState([]);
+
+  const value = {
+    navigate,
+    currency,
+    token,
+    setToken,
+    user,
+    setUser,
+    isOwner,
+    setIsOwner,
+    showLogin,
+    setShowLogin,
+    pickupDate,
+    setPickupDate,
+    returnDate,
+    setReturnDate,
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
